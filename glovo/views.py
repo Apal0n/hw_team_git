da82a1f40a3e6e91dc0dc63fb6d0b331d466cd39
Apal0n <126178887+Apal0n@users.noreply.github.com>
2023-04-20 17:59:59 +0600

    Update views.py

--- glovo/views.py
+++ glovo/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import Order
+from .models import Order, Courier
 from django.http import HttpResponse
 
 def main_page(request):
     return render(request, 'base.html')
 
 def show_couriers(request):
-    couriers = models.Courier.objects.all()
+    couriers = Courier.objects.all()
     return render(request, 'couriers.html', context={'couriers': couriers})
 
 def order():
